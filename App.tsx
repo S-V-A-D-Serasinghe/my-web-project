0632ea939c9ed95fde392bfcb4d08e60cfdb2989
S-V-A-D-Serasinghe <58882366+S-V-A-D-Serasinghe@users.noreply.github.com>
2022-11-28 09:10:32 +0530

    1 file modified

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import './style.css';
+import './mystyle.css';
 
 export default function App() {
   return (
     <div>
       <h1>Aditha Creation</h1>
+      <h2 className="tagline">Creating A new Way</h2>
       <p></p>
     </div>
   );

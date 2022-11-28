c40d9db77935afdd271c1d4bb2862069f7228ddb
S-V-A-D-Serasinghe <58882366+S-V-A-D-Serasinghe@users.noreply.github.com>
2022-11-28 12:20:35 +0530

    1 binaries uploaded

--- App.tsx
+++ App.tsx
@@ -7,19 +7,16 @@ export default function App() {
     <div>
       <h1>Aditha Creation</h1>
       <h2 className="tagline">Creating A new Way</h2>
-      <body>  
-    <div className="offers">
-        <h1>AdithaCreation</h1>
-        <img className="responsive" src="download.png" width=320px alt="Image of a programmer">
-        <h2 className="tagline">Creating A new Way</h2>
       </div>
-    
-      <p>Hello there! We are a leading sofware company in sri lanaka</strong> if you have any project please contact our helpdesk.
-      </p>
-    
-      <br/>
-      <a href="form.html" target="_blank">send your details</a></br></br>
-      
-    </div>
   );
+  import React from 'react';
+import logo from '.download.png'; // Tell webpack this JS file uses this image
+
+console.log(logo); // /logo.84287d09.png
+
+function Header() {
+  // Import result is the URL of your image
+  return <img src={logo} alt="Logo" />;
+}
+
 }

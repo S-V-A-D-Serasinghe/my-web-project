52ea8b098cfecfe3b6cc3822bbb083b9590c8e7d
S-V-A-D-Serasinghe <58882366+S-V-A-D-Serasinghe@users.noreply.github.com>
2022-11-28 12:09:57 +0530

    1 file modified, 1 added

--- App.tsx
+++ App.tsx
@@ -7,7 +7,19 @@ export default function App() {
     <div>
       <h1>Aditha Creation</h1>
       <h2 className="tagline">Creating A new Way</h2>
-      <p></p>
+      <body>  
+    <div className="offers">
+        <h1>AdithaCreation</h1>
+        <img className="responsive" src="download.png" width=320px alt="Image of a programmer">
+        <h2 className="tagline">Creating A new Way</h2>
+      </div>
+    
+      <p>Hello there! We are a leading sofware company in sri lanaka</strong> if you have any project please contact our helpdesk.
+      </p>
+    
+      <br/>
+      <a href="form.html" target="_blank">send your details</a></br></br>
+      
     </div>
   );
 }
